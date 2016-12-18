289845e65537114ad1aff1ba033d5193862577db
compagnie <edouard.hinard@laposte.net>
2016-12-18 20:54:47 +0100

    -

---
+++ UA.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+# coding: utf-8
+
+import sys
+
+import Message
+import Transaction
+
+class User:
+    def __init__(self, sipuri, password=None, invitecb=None):
+        self.sipuri = sipuri
+        self.password = password
+        self.invitecb = invitecb
+        self.registered = False
+        
+    def register(self, cb=None):
+        reg = Message.REGISTER(self.sipuri)
+        m = Transaction.manager.send(reg)
+        if m.code == 401:
+            reg.authenticate(m.headers['WWW-Authenticate'], self.password)
+            m = Transaction.manager.send(reg)
+        elif m.code == 407:
+            reg.authenticate(m.headers['Proxy-Authenticate'], self.password)
+            m = Transaction.manager.send(reg)
+
+        if m.code == 200:
+            self.registered = True
+            return self
+
+        raise Exception(str(m))
+        
+    async def invite(self, to, sdp, audioin, audioout):
+        inv = Message.INVITE()
+        m = await Transaction.manager.send(reg)
+        if m.code == 401:
+            reg.authenticate(m.headers['WWW-Authenticate'], self.password)
+            m = await Transaction.manager.send(reg)
+        elif m.code == 407:
+            reg.authenticate(m.headers['Proxy-Authenticate'], self.password)
+            m = await Transaction.manager.send(reg)
+
+        if m.code == 200:
+            self.registered = True
+            return self
+
+        raise Exception(str(m))
+
